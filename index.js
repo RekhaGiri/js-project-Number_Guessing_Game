4ed9635ba7929b9a88beae13308eebc77647d9ad
RekhaGiri <131673556+RekhaGiri@users.noreply.github.com>
2023-09-27 20:54:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// var msg1 = document.getElementById("msg1");
+// var msg2 = document.getElementById("msg2");
+// var ans = Math.floor(Math.random()*100)+1;
+// var no_of_guess = 0;
+
+// function play(){
+//     let userval = document.getElementById("num").ari
+
+// }
